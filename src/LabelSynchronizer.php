e1245f26565b8c8666f6af81785d074af281320b
Matthieu Napoli <matthieu@mnapoli.fr>
2015-02-27 11:59:27 +1300

    Split label synchronization code to separate class

---
+++ src/LabelSynchronizer.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Piwik\GithubSync;
+
+use ArrayComparator\ArrayComparator;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+/**
+ * Synchronizes labels.
+ */
+class LabelSynchronizer
+{
+    /**
+     * @var Github
+     */
+    private $github;
+
+    /**
+     * @var InputInterface
+     */
+    private $input;
+
+    /**
+     * @var OutputInterface
+     */
+    private $output;
+
+    public function __construct(Github $github, InputInterface $input, OutputInterface $output)
+    {
+        $this->github = $github;
+        $this->input = $input;
+        $this->output = $output;
+    }
+
+    public function synchronize($from, $to)
+    {
+        $fromLabels = $this->github->getLabels($from);
+        $toLabels = $this->github->getLabels($to);
+
+        $comparator = new ArrayComparator();
+
+        // Labels identity is their name, so they are the same if they have the same name
+        $comparator->setItemIdentityComparator(function ($key1, $key2, $label1, $label2) {
+            return strcasecmp($label1['name'], $label2['name']) === 0;
+        });
+        // Same labels have differences if they have a different color
+        $comparator->setItemComparator(function ($label1, $label2) {
+            return $label1['color'] === $label2['color'];
+        });
+
+        $comparator
+            ->whenDifferent(function ($label1, $label2) use ($to) {
+                $this->output->writeln(sprintf('Same label but different colors for <info>%s</info>', $label1['name']));
+
+                if ($this->confirm('Do you want to change the color of the label?')) {
+                    $this->output->writeln('<error>Not implemented yet</error>');
+                }
+            })
+            ->whenMissingRight(function ($label) use ($to) {
+                $this->output->writeln(sprintf('Missing label <info>%s</info> from %s', $label['name'], $to));
+
+                if ($this->confirm('Do you want to create this missing label?')) {
+                    $this->createLabel($to, $label['name'], $label['color']);
+                }
+            })
+            ->whenMissingLeft(function ($label) use ($to) {
+                $this->output->writeln(sprintf('Extra label <info>%s</info> in %s', $label['name'], $to));
+
+                if ($this->confirm('Do you want to <fg=red>delete</fg=red> this extra label?')) {
+                    $this->output->writeln('<error>Not implemented yet</error>');
+                }
+            });
+
+        $comparator->compare($fromLabels, $toLabels);
+    }
+
+    private function createLabel($repository, $name, $color)
+    {
+        if (! $this->github->isAuthenticated()) {
+            $this->output->writeln('<error>Impossible because you are not authenticated. You need to provide a personal access token using the "--token" option. Create a token at https://github.com/settings/applications</error>');
+            return;
+        }
+
+        $this->github->createLabel($repository, $name, $color);
+
+        $this->output->writeln('<info>Label created</info>');
+    }
+
+    private function confirm($message)
+    {
+        $helper = new QuestionHelper();
+        $question = new ConfirmationQuestion('<question>' . $message . '</question>', true);
+        return $helper->ask($this->input, $this->output, $question);
+    }
+}
--- src/SyncCommand.php
+++ src/SyncCommand.php
@@ -2,13 +2,11 @@
 
 namespace Piwik\GithubSync;
 
-use ArrayComparator\ArrayComparator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class SyncCommand extends Command
 {
@@ -49,64 +47,9 @@ class SyncCommand extends Command
 
         $output->writeln(sprintf('<comment>Synchronizing labels from %s to %s</comment>', $from, $to));
 
-        $fromLabels = $this->github->getLabels($from);
-        $toLabels = $this->github->getLabels($to);
-
-        $comparator = new ArrayComparator();
-
-        // Labels identity is their name, so they are the same if they have the same name
-        $comparator->setItemIdentityComparator(function ($key1, $key2, $label1, $label2) {
-            return strcasecmp($label1['name'], $label2['name']) === 0;
-        });
-        // Same labels have differences if they have a different color
-        $comparator->setItemComparator(function ($label1, $label2) {
-            return $label1['color'] === $label2['color'];
-        });
-
-        $comparator
-            ->whenDifferent(function ($label1, $label2) use ($input, $output, $to) {
-                $output->writeln(sprintf('Same label but different colors for <info>%s</info>', $label1['name']));
-
-                if ($this->confirm($input, $output, 'Do you want to change the color of the label?')) {
-                    $output->writeln('<error>Not implemented yet</error>');
-                }
-            })
-            ->whenMissingRight(function ($label1) use ($input, $output, $to) {
-                $output->writeln(sprintf('Missing label <info>%s</info> from %s', $label1['name'], $to));
-
-                if ($this->confirm($input, $output, 'Do you want to create this missing label?')) {
-                    $this->createLabel($output, $to, $label1['name'], $label1['color']);
-                }
-            })
-            ->whenMissingLeft(function ($label2) use ($input, $output, $to) {
-                $output->writeln(sprintf('Extra label <info>%s</info> in %s', $label2['name'], $to));
-
-                if ($this->confirm($input, $output, 'Do you want to <fg=red>delete</fg=red> this extra label?')) {
-                    $output->writeln('<error>Not implemented yet</error>');
-                }
-            });
-
-        $comparator->compare($fromLabels, $toLabels);
+        $labelSynchronizer = new LabelSynchronizer($this->github, $input, $output);
+        $labelSynchronizer->synchronize($from, $to);
 
         $output->writeln('<comment>Finished</comment>');
-    }
-
-    private function createLabel(OutputInterface $output, $repository, $name, $color)
-    {
-        if (! $this->github->isAuthenticated()) {
-            $output->writeln('<error>Impossible because you are not authenticated. You need to provide a personal access token using the "--token" option. Create a token at https://github.com/settings/applications</error>');
-            return;
-        }
-
-        $this->github->createLabel($repository, $name, $color);
-
-        $output->writeln('<info>Label created</info>');
-    }
-
-    private function confirm($input, $output, $message)
-    {
-        $helper = $this->getHelper('question');
-        $question = new ConfirmationQuestion('<question>' . $message . '</question>', true);
-        return $helper->ask($input, $output, $question);
     }
 }
